30c3dddc70b540292d4b652037e35f13e29bfbe7
richardsheridan <richard.sheridan@gmail.com>
2014-12-01 15:49:58 -0500

    module building setup tweaks

--- calc_g_zs_setup.py
+++ calc_g_zs_setup.py
@@ -5,14 +5,14 @@ import os
 
 if len(sys.argv) == 1:
     sys.argv.append('build')
+else:
+    raise TypeError('This script accepts no command line arguments, currently')
 
-#from distutils.core import setup, Extension
 from setuptools import setup, Extension
 from distutils.command.build_ext import build_ext
 
-
 from distutils.util import get_platform
-platform = '.%s-%s'%(get_platform(),sys.version[:3])
+platform = '.{}-{}'.format(get_platform(),sys.version[:3])
 
 extra_compile_args =  {'msvc': ['/EHsc']}
 extra_link_args =  {}
@@ -28,9 +28,10 @@ class build_ext_subclass(build_ext):
                 e.extra_link_args = extra_link_args[c]
         build_ext.build_extensions(self)
         
+mod_name = 'calc_g_zs_cex'
 from numpy import get_include
-calc_g_zs_module = Extension('calc_g_zs_cex',
-                             sources=['calc_g_zs_cex.c'],
+calc_g_zs_module = Extension(mod_name,
+                             sources=[mod_name+'.c'],
                              include_dirs=[get_include()],
                              )
                      
@@ -55,5 +56,5 @@ dist = setup(
         cmdclass = {'build_ext': build_ext_subclass },
         )
 
-shutil.copy('build\lib{platform}\calc_g_zs_cex.pyd'.format(platform=platform),
-            'calc_g_zs_cex.pyd')
+shutil.copy(os.path.join('build', 'lib'+platform, mod_name+'.pyd'),
+            mod_name+'.pyd')
